7b0bcbf5950203dc7536cf964e0ea90cc33879d5
Aero <aerocloud27@gmail.com>
2026-07-26 04:04:21 +0200

    Bump version 1.3.0 (versionCode 5)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "dev.aero.glyphlapse"
         minSdk = 34
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.2.0"
+        versionCode = 5
+        versionName = "1.3.0"
     }
 
     signingConfigs {
